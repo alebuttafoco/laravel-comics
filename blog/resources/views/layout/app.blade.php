c15db51ff4e11dbb30f227e6bc48cb035618a1e9
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-08 16:01:02 +0200

    add links

--- blog/resources/views/layout/app.blade.php
+++ blog/resources/views/layout/app.blade.php
@@ -15,13 +15,43 @@
     <body>
 
         {{-- SITE HEADER --}}
-        <header id="site_header">HEADER CONTENT</header>
+        <header id="header">
+            <div class="header__top">colore blu</div>
+
+            <nav class="nav">
+
+                <div class="logo">
+                    <img src="./img/dc-logo.png" alt="">
+                </div>
+
+                <div class="links">
+                    <a href=" {{route('characters')}} ">characters</a>
+                    <a href=" {{route('comics')}} ">comics</a>
+                    <a href=" {{route('movies')}} ">movies</a>
+                    <a href=" {{route('tv')}} ">tv</a>
+                    <a href=" {{route('games')}} ">games</a>
+                    <a href=" {{route('collectibles')}} ">collectibles</a>
+                    <a href=" {{route('videos')}} ">videos</a>
+                    <a href=" {{route('fans')}} ">fans</a>
+                    <a href=" {{route('news')}} ">news</a>
+                    <a href=" {{route('shop')}} ">shop</a>
+                </div>
+
+                <div class="search_box">
+                    <input type="text" name="search_input" id="search_input">
+                </div>
+
+            </nav>
+
+            <div class="jumbotron"></div>
+
+        </header>
 
         {{-- SITE_MAIN --}}
-        <main id="site_main">MAIN CONTENT</main>
+        <main id="main">MAIN CONTENT</main>
 
         {{-- SITE FOOTER --}}
-        <footer id="site_footer">FOOTER CONTENT</footer>
+        <footer id="footer">FOOTER CONTENT</footer>
 
     </body>
 </html>
