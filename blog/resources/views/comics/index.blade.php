32633e424fabf885ecd8ae112938a2c3a180ede6
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-09 13:49:11 +0200

    add features

--- blog/resources/views/comics/index.blade.php
+++ blog/resources/views/comics/index.blade.php
@@ -27,10 +27,39 @@
     </div>
     {{-- /cards --}}
 
-    {{-- btn --}}
-    <div class="row-center">
+     {{-- btn --}}
+     <div class="row-center">
         <div class="btn btn--load_more">load more</div>
     </div>
+
+    {{-- features --}}
+    <div class="features ">
+        <div class="container row-evenly">
+
+            <div class="feature">
+                <img src="./img/buy-comics-digital-comics.png" alt="">
+                <span>digital comics</span>
+            </div>
+            <div class="feature">
+                <img src="./img/buy-comics-merchandise.png" alt="">
+                <span>dc merchandise</span>
+            </div>
+            <div class="feature">
+                <img src="./img/buy-comics-subscriptions.png" alt="">
+                <span>subscription</span>
+            </div>
+            <div class="feature">
+                <img src="./img/buy-comics-shop-locator.png" alt="">
+                <span>comic shop locator</span>
+            </div>
+            <div class="feature">
+                <img src="./img/buy-dc-power-visa.svg" alt="">
+                <span>dc power visa</span>
+            </div>
+
+        </div>
+    </div>
+
 </section>
     
 
