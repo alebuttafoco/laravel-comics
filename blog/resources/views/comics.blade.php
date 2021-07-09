79f2a14da313235e14f18095283e32b102275d5e
alebuttafoco <buttafoco.alessandro@gmail.com>
2021-07-09 10:51:29 +0200

    complete cards layout

--- blog/resources/views/comics.blade.php
+++ blog/resources/views/comics.blade.php
@@ -1,1 +1,22 @@
 @extends('layout.app')
+@section('title', 'Comics Page')
+{{-- CONTENT --}}
+@section('content')
+
+<div class="comics">
+    <div class="cards">
+        <div class="container">
+        
+            @foreach ($comics as $comic)
+            <div class="card">
+                <img src=" {{$comic['thumb']}} " alt="">
+                <div class="card__text">{{($comic['series'])}}</div>
+            </div>
+            @endforeach
+        
+        </div>
+    </div>
+</div>
+    
+
+@endsection
